355a801dfdcb808183720c2cdfda874f7f45d267
Haneefah55 <olasupoomotayo@gmail.com>
2026-04-29 09:16:30 +0100

    fix eslint error

--- src/lib/connectDb.ts
+++ src/lib/connectDb.ts
@@ -6,8 +6,12 @@ export const connectDb = async () =>{
   try{
     const conn = await mongoose.connect(mongoUri)
     console.log(`Mongodb connected. ${conn.connection.host}`)
-  }catch(error: any){
-    console.log("error connecting to mongodb.",  error.message)
+  }catch(error: unknown){
+    if (error instanceof Error) {
+      console.log("error connecting to mongodb.",  error.message)
+    } else {
+      console.log("An unknown error occurred.")
+    }
     process.exit(1)
   }
 }
