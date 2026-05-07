e2821519c27d68e335c228e7b1eecfce4c80073d
Haneefah55 <olasupoomotayo@gmail.com>
2026-05-07 23:57:54 +0100

    edit next config

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,19 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  trailingSlash: false,
   eslint: {
     ignoreDuringBuilds: true,
   },
+  async redirects() {
+    return [
+      {
+        source: '/:path*/',
+        destination: '/:path*',
+        permanent: true,
+      },
+    ]
+  }
 };
 
 export default nextConfig;
